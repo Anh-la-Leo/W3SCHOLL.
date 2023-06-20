1c62ff05acdbe55bfd73869a39d8ef48563ddbb9
Anh-la-Leo <tuv675592@gmail.com>
2023-06-21 00:52:26 +0700

    active

---
+++ GBIP/script.js
@@ -0,0 +1,510 @@
+"use strict";
+const menus = [
+    {
+        "name": "Get data",
+        "order": [
+            {
+                "name_order": "",
+                "orders":[
+            {
+                "dish": "Occurrences",
+                "src" : "#"
+            },
+            {
+                "dish": "GBIF API",
+                "src" : "#"
+            },
+            {
+                "dish": "Species",
+                "src" : "#"
+            },
+            {
+                "dish": "Datasets",
+                "src" : "#"
+            },
+            {
+                "dish": "Occurrence snapshots",
+                "src" : "#"
+            },
+            {
+                "dish": "Hosted portals",
+                "src" : "#"
+            },
+            {
+                "dish": "Trends",
+                "src" : "#"
+            }
+        ]
+        }
+        ]
+    },
+    {
+        "name": "How-to",
+        "order": [
+            {
+                "name_order": "SHARE DATA",
+                "orders":[
+            {
+                "dish": "Quick-start guide",
+                "src" : "#"
+            },
+            {
+                "dish": "Dataset classes",
+                "src" : "#"
+            },
+            {
+                "dish": "Data hosting",
+                "src" : "#"
+            },
+            {
+                "dish": "Standards",
+                "src" : "#"
+            },
+            {
+                "dish": "Become a publisher",
+                "src" : "#"
+            },
+            {
+                "dish": "Data quality",
+                "src" : "#"
+            },
+            {
+                "dish": "Data papers",
+                "src" : "#"
+            }
+        ]
+        },
+        {
+            "name_order": "USE DATA",
+            "orders":[
+        {
+            "dish": "Featured data use",
+            "src" : "#"
+        },
+        {
+            "dish": "Citation guidelines",
+            "src" : "#"
+        },
+        {
+            "dish": "GBIF citations",
+            "src" : "#"
+        },
+        {
+            "dish": "Citation widget",
+            "src" : "#"
+        }
+    ]
+    }
+        ]
+    },
+    {
+        "name": "Tools",
+        "order": [
+            {
+                "name_order": "IPT",
+                "orders":[
+            {
+                "dish": "IPT",
+                "src" : "#"
+            },
+            {
+                "dish": "Data validator",
+                "src" : "#"
+            },
+            {
+                "dish": "Scientific Collections",
+                "src" : "#"
+            },
+            {
+                "dish": "Suggest a dataset",
+                "src" : "#"
+            },
+            {
+                "dish": "New data model ⭐",
+                "src" : "#"
+            }
+        ]
+        },
+        {
+            "name_order": "DATA ACCESS AND USE",
+            "orders":[
+        {
+            "dish": "Hosted portals",
+            "src" : "#"
+        },
+        {
+            "dish": "Data processing",
+            "src" : "#"
+        },
+        {
+            "dish": "Derived datasets",
+            "src" : "#"
+        },
+        {
+            "dish": "rgbif",
+            "src" : "#"
+        }, 
+        {
+            "dish": "pygbif",
+            "src" : "#"
+        },
+         {
+            "dish": "MAXENT",
+            "src" : "#"
+        },
+        {
+            "dish": "Tools câtlogue",
+            "src" : "#"
+        }
+       ]
+       },
+       {
+        "name_order": "GBIF LABS",
+        "orders":[
+    {
+        "dish": "Species matching",
+        "src" : "#"
+    },
+    {
+        "dish": "Name parser",
+        "src" : "#"
+    },
+    {
+        "dish": "Sequence ID",
+        "src" : "#"
+    },
+    {
+        "dish": "Relative observation trends",
+        "src" : "#"
+    }, 
+    {
+        "dish": "GBIF data blog",
+        "src" : "#"
+    }
+   ]
+   }
+        ]
+
+    },
+    {
+        "name": "Community",
+        "order": [
+            {
+                "name_order": "NETWORK",
+                "orders":[
+            {
+                "dish": "Participant network",
+                "src" : "#"
+            },
+            {
+                "dish": "Nodes",
+                "src" : "#"
+            },
+            {
+                "dish": "Publishers",
+                "src" : "#"
+            },
+            {
+                "dish": "Network contacts",
+                "src" : "#"
+            },
+            {
+                "dish": "Community forum",
+                "src" : "#"
+            },
+            {
+                "dish": "alliance for biodiversity",
+                "src" : "#"
+            },
+           
+        ]
+        },
+        {
+            "name_order": "VOLUNTEERS",
+            "orders":[
+        {
+            "dish": "Mentors",
+            "src" : "#"
+        },
+        {
+            "dish": "Ambassadors",
+            "src" : "#"
+        },
+        {
+            "dish": "Translators",
+            "src" : "#"
+        },
+        {
+            "dish": "Citizen scientists",
+            "src" : "#"
+        }
+    ]
+    },
+    {
+        "name_order": "ACTIVITIES",
+        "orders":[
+    {
+        "dish": "Capacity enhancement",
+        "src" : "#"
+    },
+    {
+        "dish": "Programes & projects",
+        "src" : "#"
+    },
+    {
+        "dish": "Tranining and learning resources",
+        "src" : "#"
+    },
+    {
+        "dish": "Data Use Club",
+        "src" : "#"
+    },
+    {
+        "dish": "Living Atlases",
+        "src" : "#"
+    }
+]
+}
+        ]
+    },
+    {
+        "name": "About",
+        "order": [
+            {
+                "name_order": "INSIDE GBIF",
+                "orders":[
+            {
+                "dish": "What is GBIF?",
+                "src" : "#"
+            },
+            {
+                "dish": "Become a member",
+                "src" : "#"
+            },
+            {
+                "dish": "Governance",
+                "src" : "#"
+            },
+            {
+                "dish": "Implementation plan",
+                "src" : "#"
+            },
+            {
+                "dish": "Work Programme",
+                "src" : "#"
+            },
+            {
+                "dish": "Partnerships",
+                "src" : "#"
+            },
+            {
+                "dish": "Release notes",
+                "src" : "#"
+            },
+            {
+                "dish": "Contacts",
+                "src" : "#"
+            }
+        ]
+        }, {
+            "name_order": "NEWS & OUTREACH",
+            "orders":[
+        {
+            "dish": "News",
+            "src" : "#"
+        },
+        {
+            "dish": "Newsletters and lists",
+            "src" : "#"
+        },
+        {
+            "dish": "Events",
+            "src" : "#"
+        },
+        {
+            "dish": "Awards",
+            "src" : "#"
+        },
+        {
+            "dish": "Science Review",
+            "src" : "#"
+        },
+        {
+            "dish": "Data use",
+            "src" : "#"
+        }
+    ]
+    }
+        ]
+    }
+];
+const img = [
+    "url('img/backgrourd.png')",
+    "url('img/backgrourd2.png')",
+    "url('img/backgrourd3.png')",
+    "url('img/backgrourd4.png')",
+    "url('img/backgrourd.png')",
+    "url('img/backgrourd2.png')",
+    "url('img/backgrourd3.png')",
+    "url('img/backgrourd4.png)",
+    "url('img/backgrourd2.png)",
+    "url('img/backgrourd3.png)"
+];
+const header = document.querySelector("#header");
+const logo = document.querySelector("#logo");
+const inputLogin = document.querySelector("#input-login");
+const loginButton =document.querySelector("#login-button");
+const registerButton = document.querySelector("#register-button");
+const menu = document.querySelector("#menu");
+const orders = document.querySelector("#orders");
+const buttonClose = document.querySelector("#close");
+const buttonLogin = document.querySelector("#button_login");
+const login = document.querySelector("#login");
+const articleSearch = document.querySelector("#article-search");
+const menuRight = document.querySelector("#menu_right");
+let nodeList =[];
+let click_on = -1;
+let add = null;
+let element = document.getElementById('main');
+    let style = window.getComputedStyle(element);
+ let   value = style.getPropertyValue('top');
+
+ 
+let show = () => {
+let txt = "";
+for (let i  =0;i < menus.length;i++) {  
+    txt  += `<button class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2 button_menu buttonMenu"  onclick="showOrder(${i},this)">${menus[i].name}</button>`;  
+    menu.innerHTML=txt;
+    }
+   
+};
+
+let showOrder=(x,y)=> {
+    if (x == click_on) {
+        click_on = -1;
+        orders.innerHTML = "";
+        y.style.background = "none";
+        return;
+    }
+    click_on = x;
+    let txt =  "";
+    for (let i = 0;i < menus[x].order.length;i++) {
+       txt += `<div class="order_non"><ul class="list-group"><p class="name_order">&emsp; ${menus[x].order[i].name_order}</p>`;    
+      for(let j = 0;j< menus[x].order[i].orders.length;j++) {
+        txt +=`<li class="list-group-item"><a href="${menus[x].order[i].orders[j].src}" class="text-dark">${menus[x].order[i].orders[j].dish}</a></li>`;
+      }   
+      txt += "</ul></div>";
+    }
+    let a = x  * 89 + 115 + "px";
+    orders.style.marginLeft = a;
+  orders.innerHTML = txt;
+  if (addNode(y)) {
+    nodeList.push(y);
+  } 
+  for (let i = 0;i< nodeList.length;i++) {   
+        nodeList[i].style.background = "none";
+  }
+  y.style.backgroundColor = "orange";
+}
+let no_order = () => {
+    orders.innerHTML = "";
+    click_on =-1;
+    for(let x = 0;x < nodeList.length;x++) {
+        nodeList[x].style.background = "none";
+    }
+
+}
+let addNode = (x) => {
+    for (let i = 0;i < nodeList.length;i++) {
+        if (nodeList[i] == x) {
+            return false;
+        }
+    }
+    return true;
+}
+let randomBackground = ()=> {
+    let x= Math.floor(Math.random() * 10);
+    header.style.background =img[x];
+    header.style.backgroundSize ="cover";
+}
+randomBackground();
+loginButton.addEventListener("click",()=>{
+    loginButton.style.borderBottom = "2px solid seagreen";
+    loginButton.style.color = "seagreen";
+    registerButton.style.borderBottom = "2px solid lightgrey";
+    registerButton.style.color = "lightgrey";
+    inputLogin.innerHTML = `<label class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">USERNAME OR EMAIL</label>
+    <input class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 "  type="text" data-bs-toggle="tooltip" title="Please fill out this field.">
+    <label class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">PASSWORD</label>
+    <input class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12" type="text" data-bs-toggle="tooltip" title="Please fill out this field.">
+    <button class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 " id="singin">SINGIN</button>
+  `;
+})
+registerButton.addEventListener("click",()=>{
+    registerButton.style.borderBottom = "2px solid seagreen";
+    registerButton.style.color = "seagreen";
+    loginButton.style.borderBottom = "2px solid lightgrey";
+    loginButton.style.color = "lightgrey";
+    inputLogin.innerHTML = `<label class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">COUNTRY</label>
+    <input class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 "  type="text" data-bs-toggle="tooltip" title="Please fill out this field." value="VIET NAM">
+    <label class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">EMAIL</label>
+    <input class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12" type="text" data-bs-toggle="tooltip" title="Please fill out this field.">
+    <label class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">USERNAME</label>
+    <input class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 "  type="text" data-bs-toggle="tooltip" title="Please fill out this field.">
+    <label class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">PASSWORD</label>
+    <input class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12" type="text" data-bs-toggle="tooltip" title="Please fill out this field.">
+    <button class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 " id="singin">NEXT</button>`;
+})
+
+//   ()
+buttonLogin.addEventListener("click",()=> {
+
+login.style.display = "block";
+login.style.marginTop = "-50px";
+for (let i =1;i<= 50;i++) {
+setTimeout(()=>{
+    let a = 50 - i +580;
+    login.style.marginTop = `-${a}px`;
+},i * 20);
+}
+})
+buttonClose.addEventListener("click",()=> {
+    login.style.display = "none"; 
+});
+menu.addEventListener("click",()=> {
+    login.style.display = "none"; 
+});
+logo.addEventListener("click",()=> {
+    login.style.display = "none"; 
+});
+articleSearch.addEventListener("click",()=> {
+    login.style.display = "none"; 
+});
+header.addEventListener("click",()=> {
+    login.style.display = "none"; 
+});
+
+// 
+let setPlay3s = () => {
+    setTimeout(()=>{
+        show();
+    },3000);
+
+}
+
+let setMenuRight = () => {
+    setTimeout(()=>{
+        menuRight.style.visibility= "visible";
+    },4800);
+}
+setPlay3s();
+setMenuRight();
+
+
+
+
+
+
+
+
+
